604de6c11548b5e5b595f93e5beb54bbfba151d0
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-25 17:12:08 +0200

    Add hotel detail screen

---
+++ lib/screens/hotel_detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HotelDetailScreen extends StatefulWidget {
+  const HotelDetailScreen({super.key});
+
+  @override
+  State<HotelDetailScreen> createState() => _HotelDetailScreenState();
+}
+
+class _HotelDetailScreenState extends State<HotelDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hotel Detail");
+  }
+}
