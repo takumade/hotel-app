fd6fcaed1550663453be711b656e22d0b19c4c75
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-26 23:35:04 +0200

    change icon

--- lib/widgets/general/app_bar.dart
+++ lib/widgets/general/app_bar.dart
@@ -83,7 +83,7 @@ class CustomAppBar extends StatelessWidget {
                       },
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Icon(Icons.shopping_basket),
+                        child: Icon(Icons.shopping_bag_outlined),
                       ),
                     ),
                   ),
