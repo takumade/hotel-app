d43268508eed777292298275f2a3690fc0d2667f
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-26 23:42:04 +0200

    Refactor

--- lib/themes/hotel_app_theme.dart
+++ lib/themes/hotel_app_theme.dart
@@ -30,22 +30,19 @@ class HotelAppTheme {
     );
     final ThemeData base = ThemeData.light();
     return base.copyWith(
-      colorScheme: colorScheme,
       primaryColor: primaryColor,
       indicatorColor: Colors.white,
       splashColor: Colors.white24,
       splashFactory: InkRipple.splashFactory,
       canvasColor: Colors.white,
-      backgroundColor: const Color(0xFFFFFFFF),
       scaffoldBackgroundColor: const Color(0xFFF6F6F6),
-      errorColor: const Color(0xFFB00020),
       buttonTheme: ButtonThemeData(
         colorScheme: colorScheme,
         textTheme: ButtonTextTheme.primary,
       ),
       textTheme: _buildTextTheme(base.textTheme),
       primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
-      platform: TargetPlatform.iOS,
+      platform: TargetPlatform.iOS, colorScheme: colorScheme.copyWith(background: const Color(0xFFFFFFFF)).copyWith(error: const Color(0xFFB00020)),
     );
   }
 }
