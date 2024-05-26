e46cf455359b581e078c3691742513f74c8072de
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-26 19:31:23 +0200

    Add some basic

--- lib/screens/hotel_detail_screen.dart
+++ lib/screens/hotel_detail_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hotel_app/model/hotel_list_data.dart';
 
+import 'package:hotel_app/widgets/general/app_bar.dart';
+import '../themes/hotel_app_theme.dart';
+
 class HotelDetailScreen extends StatefulWidget {
   final HotelListData hotel;
+   final ScrollController _scrollController = ScrollController();
   
   HotelDetailScreen({super.key, required this.hotel});
 
@@ -13,6 +17,47 @@ class HotelDetailScreen extends StatefulWidget {
 class _HotelDetailScreenState extends State<HotelDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text(widget.hotel.titleTxt);
+    return Theme(
+      data: HotelAppTheme.buildLightTheme(),
+      child: Container(
+        child: Scaffold(
+          body: Stack(
+            children: <Widget>[
+              InkWell(
+                splashColor: Colors.transparent,
+                focusColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                hoverColor: Colors.transparent,
+                onTap: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                },
+                child: Column(
+                  children: <Widget>[
+                    CustomAppBar(),
+                    Expanded(
+                      child: NestedScrollView(
+                        controller: widget._scrollController,
+                        headerSliverBuilder:
+                            (BuildContext context, bool innerBoxIsScrolled) {
+                          return <Widget>[
+                           
+                          ];
+                        },
+                        body: Container(
+                          color:
+                              HotelAppTheme.buildLightTheme().backgroundColor,
+                          child: Text('Hotels Details')
+                          ),
+                        ),
+                      ),
+                    
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
