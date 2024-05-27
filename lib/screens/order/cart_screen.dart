6cb850c6f59de2176d61e8d9e452c75e28b1a1aa
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-27 15:16:42 +0200

    Finish cart screen

--- lib/screens/order/cart_screen.dart
+++ lib/screens/order/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:hotel_app/themes/hotel_app_theme.dart';
 import 'package:hotel_app/widgets/general/app_bar.dart';
 
@@ -14,40 +15,59 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Theme(
-      data: HotelAppTheme.buildLightTheme(),
-      child: Container(
-        child: Scaffold(
-          backgroundColor: HotelAppTheme.buildLightTheme().canvasColor,
-          body: Stack(
-            children: <Widget>[
-              InkWell(
-                splashColor: Colors.transparent,
-                focusColor: Colors.transparent,
-                highlightColor: Colors.transparent,
-                hoverColor: Colors.transparent,
-                onTap: () {
-                  FocusScope.of(context).requestFocus(FocusNode());
-                },
-                child: Column(children: <Widget>[
-                  CustomAppBar(title: "Cart"),
-                  Expanded(
-                      child: ListView(
-                    children: [
-                      CartItem(
-                          text: "Meikles Hotel",
-                          imageUrl: "imageUrl",
-                          subtitle: "24 Feb to 24 May (2024)",
-                          onPressed: () {}),
-                     
-                    ],
-                  ))
-                ]),
-              ),
-            ],
+        data: HotelAppTheme.buildLightTheme(),
+        child: Container(
+          child: Scaffold(
+            backgroundColor: HotelAppTheme.buildLightTheme().canvasColor,
+            body: Column(children: <Widget>[
+              CustomAppBar(title: "Cart"),
+              Expanded(
+                  child: ListView(
+                children: [
+                  CartItem(
+                      text: "Meikles Hotel",
+                      imageUrl: "imageUrl",
+                      subtitle: "24 Feb to 24 May (2024)",
+                      onPressed: () {}),
+                  CartItem(
+                      text: "Meikles Hotel",
+                      imageUrl: "imageUrl",
+                      subtitle: "24 Feb to 24 May (2024)",
+                      onPressed: () {}),
+                  CartItem(
+                      text: "Meikles Hotel",
+                      imageUrl: "imageUrl",
+                      subtitle: "24 Feb to 24 May (2024)",
+                      onPressed: () {}),
+                  CartItem(
+                      text: "Meikles Hotel",
+                      imageUrl: "imageUrl",
+                      subtitle: "24 Feb to 24 May (2024)",
+                      onPressed: () {}),
+                  CartItem(
+                      text: "Meikles Hotel",
+                      imageUrl: "imageUrl",
+                      subtitle: "24 Feb to 24 May (2024)",
+                      onPressed: () {}),
+                  CartItem(
+                      text: "Meikles Hotel",
+                      imageUrl: "imageUrl",
+                      subtitle: "24 Feb to 24 May (2024)",
+                      onPressed: () {}),
+              
+                ],
+              )),
+                  Container(
+                    width: 220,
+                      padding: EdgeInsets.only(
+                          top: 12,  bottom: 30),
+                      child: ElevatedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.shopping_cart),
+                          label: Text("Checkout")))
+            ]),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
 
@@ -71,93 +91,121 @@ class CartItem extends StatelessWidget {
       onTap: onPressed,
       child: Container(
         width: MediaQuery.of(context).size.width,
-        height: 90,
-        padding: const EdgeInsets.all(10.0),
-        margin: EdgeInsets.only(left: 16, right: 16.0),
+        height: 160,
+        padding: const EdgeInsets.all(12.0),
+        margin: EdgeInsets.only(top: 12, left: 16, right: 16.0),
         decoration: BoxDecoration(
-                                          color: HotelAppTheme.buildLightTheme()
-                                              .colorScheme
-                                              .background,
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(18)),
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color:
-                                                  Colors.grey.withOpacity(0.5),
-                                              spreadRadius: 5,
-                                              blurRadius: 7,
-                                              offset: Offset(0,
-                                                  3), // changes position of shadow
-                                            ),
-                                          ],
-                                        ),
-        child: Row(
-          children: [
-            ClipRRect(
-                borderRadius: BorderRadius.circular(9.0),
-                child: Image.network(
-                    "https://images.unsplash.com/photo-1625244724120-1fd1d34d00f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                    height: 59,
-                    fit: BoxFit.cover)),
-            const SizedBox(
-              width: 15,
+          color: HotelAppTheme.buildLightTheme().colorScheme.background,
+          borderRadius: BorderRadius.all(Radius.circular(18)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
             ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(text,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  )),
-              Text(
-                subtitle,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                    color: Colors.grey,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 12),
-              ),
-              Text(
-                "3 people",
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                    color: Colors.grey,
-                    fontWeight: FontWeight.normal,
-                    fontSize: 12),
-              ),
-                ],
-              
-              
-                
-              ),
-            ),
-            Column(
-              children: [
-                Text(
-                  "\$55",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: HotelAppTheme.buildLightTheme().primaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30),
-                ),
-                Text(
-              "/night",
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                  color: Colors.grey,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 12),
-            ),
- 
-              ],
-            ),
-            
           ],
+        ),
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  // ClipRRect(
+                  //     borderRadius: BorderRadius.circular(9.0),
+                  //     child: Image.network(
+                  //         "https://images.unsplash.com/photo-1625244724120-1fd1d34d00f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  //         height: 59,
+                  //         fit: BoxFit.cover)),
+
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(text,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            )),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.date_range,
+                                color: Colors.black87.withOpacity(0.5)),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              subtitle,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.people,
+                              color: Colors.black87.withOpacity(0.5),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "3 people",
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "\$55",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: HotelAppTheme.buildLightTheme().primaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30),
+                      ),
+                      Text(
+                        "/night",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 12),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              TextButton.icon(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red)),
+                  icon: Icon(Icons.delete_outline_rounded),
+                  label: Text("Remove"))
+            ],
+          ),
         ),
       ),
     );
