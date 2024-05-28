da259d9cac0b843925f339509684a96ea7fb5136
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-29 00:31:18 +0200

    Add another option

--- lib/widgets/general/custom_titles.dart
+++ lib/widgets/general/custom_titles.dart
@@ -4,15 +4,17 @@ import 'package:flutter/material.dart';
 class CustomScreenTitle extends StatelessWidget {
 
   final String title;
+  final double top;
   const CustomScreenTitle({
     super.key,
     required this.title,
+    this.top = 20
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only(top: 20),
+        margin: EdgeInsets.only(top: top),
         child: Text(title,
             style: TextStyle(
                 fontSize: 20,
