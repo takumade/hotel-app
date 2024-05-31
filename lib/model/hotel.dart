32157598de7c988f75fea49abf87c5d49308e15b
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-31 23:48:42 +0200

    Add more fields

--- lib/model/hotel.dart
+++ lib/model/hotel.dart
@@ -9,8 +9,13 @@ class Hotel {
   final double dist;
   final double rating;
   final int reviews;
+  final List<String> facilities;
+  final String about;
+  final String address;
+  final String? date;
+  final int? people;
   final int perNight;
 
-  Hotel({required this.imagePath, required this.titleTxt, required this.subTxt, required this.dist, required this.rating, required this.reviews, required this.perNight});
+  Hotel(this.facilities, this.about, this.address, this.date, this.people, {required this.imagePath, required this.titleTxt, required this.subTxt, required this.dist, required this.rating, required this.reviews, required this.perNight});
 
 }
