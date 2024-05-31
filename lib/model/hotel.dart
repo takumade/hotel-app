c4dd894bb8f7e6294f183fb6476c9b206afe0940
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-31 23:34:59 +0200

    Add hotel model

---
+++ lib/model/hotel.dart
@@ -0,0 +1,16 @@
+
+
+
+class Hotel {
+
+  final String imagePath;
+  final String titleTxt;
+  final String subTxt;
+  final double dist;
+  final double rating;
+  final int reviews;
+  final int perNight;
+
+  Hotel({required this.imagePath, required this.titleTxt, required this.subTxt, required this.dist, required this.rating, required this.reviews, required this.perNight});
+
+}
