3b39321043cd09843f56b25ac04b5d6cccb199f7
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-31 23:56:43 +0200

    aDD MORE FIELDS

--- lib/model/cart.dart
+++ lib/model/cart.dart
@@ -11,48 +11,63 @@ class Cart extends ChangeNotifier {
   static List<Hotel> hotelList = <Hotel>[
     Hotel(
       imagePath: 'assets/hotel/hotel_1.png',
-      titleTxt: 'Grand Royal Hotel',
+      titleTxt: 'The Meikles',
       subTxt: 'Wembley, London',
+      address: "Third Street, & Jason Moyo Ave, Harare",
+      about: "Nestled in the city of sunshine, Hyatt Regency Harare The Meikles offers iconic architecture, charm, luxury, and easy access to major attractions.",
       dist: 2.0,
       reviews: 80,
       rating: 4.4,
       perNight: 180,
+      facilities: ['Swimming Pool', 'Wifi', 'Safari', 'Show']
     ),
     Hotel(
       imagePath: 'assets/hotel/hotel_2.png',
-      titleTxt: 'Queen Hotel',
+      titleTxt: 'Rainbow Towers',
       subTxt: 'Wembley, London',
+      address: "1 Pennefather Ave, Samora Machel Ave, Harare",
+      about: "Situated in Zimbabwe's sunny capital Harare, it is a golden icon on the city skyline. The luxurious 304 roomed Rainbow Towers Hotel & International Conference ",
       dist: 4.0,
       reviews: 74,
       rating: 4.5,
       perNight: 200,
+       facilities: ['Swimming Pool', 'Wifi', 'Safari', 'Show']
     ),
     Hotel(
       imagePath: 'assets/hotel/hotel_3.png',
       titleTxt: 'Grand Royal Hotel',
       subTxt: 'Wembley, London',
+      address: "1 Pennefather Ave, Samora Machel Ave, Harare",
+      about: "Situated in Zimbabwe's sunny capital Harare, it is a golden icon on the city skyline. The luxurious 304 roomed Rainbow Towers Hotel & International Conference ",
       dist: 3.0,
       reviews: 62,
       rating: 4.0,
       perNight: 60,
+      facilities: ['Swimming Pool', 'Wifi', 'Safari', 'Show']
     ),
     Hotel(
       imagePath: 'assets/hotel/hotel_4.png',
       titleTxt: 'Queen Hotel',
       subTxt: 'Wembley, London',
+      address: "1 Pennefather Ave, Samora Machel Ave, Harare",
+      about: "Situated in Zimbabwe's sunny capital Harare, it is a golden icon on the city skyline. The luxurious 304 roomed Rainbow Towers Hotel & International Conference ",
       dist: 7.0,
       reviews: 90,
       rating: 4.4,
       perNight: 170,
+      facilities: ['Swimming Pool', 'Wifi', 'Safari', 'Show']
     ),
     Hotel(
       imagePath: 'assets/hotel/hotel_5.png',
       titleTxt: 'Grand Royal Hotel',
       subTxt: 'Wembley, London',
+      address: "1 Pennefather Ave, Samora Machel Ave, Harare",
+      about: "Situated in Zimbabwe's sunny capital Harare, it is a golden icon on the city skyline. The luxurious 304 roomed Rainbow Towers Hotel & International Conference ",
       dist: 2.0,
       reviews: 240,
       rating: 4.5,
       perNight: 200,
+      facilities: ['Swimming Pool', 'Wifi', 'Safari', 'Show']
     ),
   ];
 
--- lib/model/hotel.dart
+++ lib/model/hotel.dart
@@ -12,10 +12,10 @@ class Hotel {
   final List<String> facilities;
   final String about;
   final String address;
-  final String? date;
-  final int? people;
+  String? date;
+  int? people;
   final int perNight;
 
-  Hotel(this.facilities, this.about, this.address, this.date, this.people, {required this.imagePath, required this.titleTxt, required this.subTxt, required this.dist, required this.rating, required this.reviews, required this.perNight});
+  Hotel({required this.imagePath, required this.titleTxt, required this.subTxt, required this.dist, required this.rating, required this.reviews, required this.facilities, required this.about, required this.address, required this.perNight});
 
 }
