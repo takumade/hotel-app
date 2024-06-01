a1e184c86fd751a647a487c7b3128b91ffe9085c
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-01 17:20:56 +0200

    Add more images

--- lib/model/cart.dart
+++ lib/model/cart.dart
@@ -10,64 +10,64 @@ class Cart extends ChangeNotifier {
 
   static List<Hotel> hotelList = <Hotel>[
     Hotel(
-      imagePath: 'assets/hotel/hotel_1.png',
-      titleTxt: 'The Meikles',
-      subTxt: 'Wembley, London',
+      imagePath: 'assets/hotel/meikles.jpg',
+      titleTxt: 'Meikles Hotel',
+      subTxt: 'Harare, Zimbabwe',
       address: "Third Street, & Jason Moyo Ave, Harare",
       about: "Nestled in the city of sunshine, Hyatt Regency Harare The Meikles offers iconic architecture, charm, luxury, and easy access to major attractions.",
       dist: 2.0,
-      reviews: 80,
+      reviews: 2534,
       rating: 4.4,
-      perNight: 180,
-      facilities: ['Swimming Pool', 'Wifi', 'Safari', 'Show']
+      perNight: 169,
+      facilities: ['Pool', 'Wifi', 'Safari', 'Show', 'Free Parking', 'Breakfast']
     ),
     Hotel(
-      imagePath: 'assets/hotel/hotel_2.png',
+      imagePath: 'assets/hotel/rainbow.jpg',
       titleTxt: 'Rainbow Towers',
-      subTxt: 'Wembley, London',
+      subTxt: 'Harare, Zimbabwe',
       address: "1 Pennefather Ave, Samora Machel Ave, Harare",
       about: "Situated in Zimbabwe's sunny capital Harare, it is a golden icon on the city skyline. The luxurious 304 roomed Rainbow Towers Hotel & International Conference ",
       dist: 4.0,
       reviews: 74,
-      rating: 4.5,
+      rating: 4.1,
       perNight: 200,
-       facilities: ['Swimming Pool', 'Wifi', 'Safari', 'Show']
+       facilities: ['Pool', 'Wifi', 'Safari', 'Show', 'Free Parking', 'Breakfast']
     ),
     Hotel(
       imagePath: 'assets/hotel/hotel_3.png',
-      titleTxt: 'Grand Royal Hotel',
-      subTxt: 'Wembley, London',
-      address: "1 Pennefather Ave, Samora Machel Ave, Harare",
-      about: "Situated in Zimbabwe's sunny capital Harare, it is a golden icon on the city skyline. The luxurious 304 roomed Rainbow Towers Hotel & International Conference ",
+      titleTxt: 'Monomotapa Hotel',
+      subTxt: 'Harare, Zimbabwe',
+      address: " 54 Park Ln, Harare",
+      about: "Located 100m from the country's main financial and corporate district and boasting an array of fully equipped conference rooms, ",
       dist: 3.0,
       reviews: 62,
-      rating: 4.0,
-      perNight: 60,
-      facilities: ['Swimming Pool', 'Wifi', 'Safari', 'Show']
+      rating: 4.1,
+      perNight: 125,
+      facilities: ['Pool', 'Wifi', 'Safari', 'Show', 'Free Parking', 'Breakfast']
     ),
     Hotel(
-      imagePath: 'assets/hotel/hotel_4.png',
-      titleTxt: 'Queen Hotel',
-      subTxt: 'Wembley, London',
-      address: "1 Pennefather Ave, Samora Machel Ave, Harare",
-      about: "Situated in Zimbabwe's sunny capital Harare, it is a golden icon on the city skyline. The luxurious 304 roomed Rainbow Towers Hotel & International Conference ",
+      imagePath: 'assets/hotel/bronte.jpg',
+      titleTxt: 'The Bronte Garden Hotel',
+      subTxt: 'Harare, Zimbabwe',
+      address: "Baines Ave, Harare",
+      about: "Centrally located in the Avenues, within walking distance of downtown Harare, The Bronte offers well appointed rooms and executive suites in an idyllic garden ",
       dist: 7.0,
-      reviews: 90,
-      rating: 4.4,
-      perNight: 170,
-      facilities: ['Swimming Pool', 'Wifi', 'Safari', 'Show']
+      reviews: 1682,
+      rating: 4.2,
+      perNight: 156,
+      facilities: ['Pool', 'Wifi', 'Safari', 'Show', 'Free Parking', 'Breakfast']
     ),
     Hotel(
-      imagePath: 'assets/hotel/hotel_5.png',
-      titleTxt: 'Grand Royal Hotel',
+      imagePath: 'assets/hotel/cresta.jpg',
+      titleTxt: 'Cresta Lodge',
       subTxt: 'Wembley, London',
-      address: "1 Pennefather Ave, Samora Machel Ave, Harare",
-      about: "Situated in Zimbabwe's sunny capital Harare, it is a golden icon on the city skyline. The luxurious 304 roomed Rainbow Towers Hotel & International Conference ",
+      address: "5482+G98, A5, Harare",
+      about: "Warm African hospitality. Set in tranquil surroundings on the outskirts of the country's dynamic capital is the beautiful Cresta Lodge - Harare ",
       dist: 2.0,
-      reviews: 240,
+      reviews: 1682,
       rating: 4.5,
-      perNight: 200,
-      facilities: ['Swimming Pool', 'Wifi', 'Safari', 'Show']
+      perNight: 96,
+      facilities: ['Pool', 'Wifi', 'Safari', 'Show', 'Free Parking', 'Breakfast']
     ),
   ];
 
